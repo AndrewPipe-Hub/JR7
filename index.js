84b4bdf16b9399ec7aebacab343fa3f45908e27b
AndrewPipe-Hub <andrewpipe2207@gmail.com>
2025-07-03 12:53:33 -0500

    Primer adelanto

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+//Boton de menu para navegación en pantallas pequeñas
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
 let menuOpen = false;
-
 // Toggle icon y navbar
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
@@ -26,4 +26,47 @@ document.addEventListener('click', (e) => {
         menuIcon.classList.remove('bx-x');
         menuOpen = false;
     }
+});
+
+
+
+
+
+
+//Efecto :after de la navbar
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+    let scrollY = window.pageYOffset;
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute("id");
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                if (link.getAttribute("href") === `#${sectionId}`) {
+                    link.classList.add("active");
+                }
+            });
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Typed('#typed-text', {
+        strings: [
+            'Donde podrás construir el cuerpo de tus sueños.',
+            'Entrena con los mejores profesionales.',
+            '¡Transforma tu vida hoy en JR7!'
+        ],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 2000,
+        loop: true
+    });
 });
